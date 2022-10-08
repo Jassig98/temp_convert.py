6c22a518aa4742a2019212624732aa4d20dd9f52
jasmi_ugm7gn6 <singjasm@oregonstate.edu>
2022-10-07 17:55:28 -0700

    Temp convert

---
+++ main.py
@@ -0,0 +1,9 @@
+# Author: Jasmine Singh
+# GitHub username: Jassig98
+# Date: October 6, 2022
+# Description: temp_convert.py
+
+print("Please enter a Celsius temperature.")
+num_1 = float(input())
+print("The equivalent Fahrenheit temperature is:")
+print(((9/5)*num_1)+32)
